9d3d527287df4fee3446760cf3411cff41a4010b
Shaohua Zhang <shaohua@wustl.edu>
2020-05-21 16:46:38 -0500

    react

---
+++ src/main/resources/static/precache-manifest.ef7793fffa2ef774ddbffa327b07677d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90b0ef40f1c9ff8b58c01aeb6ad1ec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe28b05075b5448711b9",
+    "url": "/static/css/2.8f4dfbca.chunk.css"
+  },
+  {
+    "revision": "065efb26f65f4ce91bff",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "fe28b05075b5448711b9",
+    "url": "/static/js/2.d83f4d30.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.d83f4d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065efb26f65f4ce91bff",
+    "url": "/static/js/main.ba13b362.chunk.js"
+  },
+  {
+    "revision": "c42518a57ae88aee800e",
+    "url": "/static/js/runtime-main.4a66c584.js"
+  }
+]);
